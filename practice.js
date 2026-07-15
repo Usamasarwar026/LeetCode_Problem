fd3f333024058e148ba30de242f79705833640c9
usamasarwar026 <usamasarwar160492@gmail.com>
2026-07-15 23:32:52 +0500

    js practice

---
+++ practice.js
@@ -0,0 +1,28 @@
+// let num = 1;
+// let a = prompt("enter the word");
+// console.log(a);
+
+// let city = ["fsd", "lhr", "karachi", "islamabad"];
+
+// const c = city.pop()
+// const c = city.push("nigeria")
+// const c = city.shift()
+// const c = city.unshift("nigeria")
+// const c = city.splice(2,"silakot","smd")
+// const c = city.splice(2,1,"silakot","smd")
+// const c = city.slice(2,3)
+
+// console.log(c);
+// console.log("city",city)
+
+
+// let city = ["fsd", "lhr", "karachi", "islamabad"];
+
+// for (let index = 0; index < city.length; index++) {
+//     const element = city[index];
+//     if (element === "islamabad") {
+//         console.log("good match at index",index)
+//     }else{
+//         console.log("bad match")
+//     }
+// }
